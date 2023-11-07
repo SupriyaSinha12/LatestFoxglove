684f6a95cb5e20d4ead3084d92eb63f3bc9babac
Sebastien Doumeizel <sebadoum@kth.se>
2023-11-07 10:25:56 +0100

    Steering Panel added

--- myExtensionName/src/index.ts
+++ myExtensionName/src/index.ts
@@ -1,9 +1,10 @@
 import { ExtensionContext } from "@foxglove/studio";
 import { initExamplePanel } from "./ExamplePanel";
 import { initCanPanel } from "./Panels/CanPanel/CanPanel";
-
+import { initSteering } from "./Panels/Steering/Steering";
 
 export function activate(extensionContext: ExtensionContext): void {
   extensionContext.registerPanel({ name: "example-panel", initPanel: initExamplePanel });
   extensionContext.registerPanel({ name: "Can Panel", initPanel: initCanPanel });
+  extensionContext.registerPanel({ name: "Steering Panel", initPanel: initSteering });
 }
